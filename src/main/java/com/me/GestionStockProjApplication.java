e9adf337786bd48fa78988709bf7093f65a4edc3
abdelMor <maraou99@gmail.com>
2019-01-22 20:41:44 -0500

    first commit

---
+++ src/main/java/com/me/GestionStockProjApplication.java
@@ -0,0 +1,14 @@
+package com.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockProjApplication.class, args);
+	}
+
+}
+
