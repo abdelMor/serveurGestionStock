9f6d73205758effaf9a3f2ba54c515a5d9b82695
abdelMor <maraou99@gmail.com>
2019-02-01 22:22:51 -0500

    second commit

---
+++ gestionStockProduitProj/src/main/java/me/gestionStock/GestionStockProduitProjApplication.java
@@ -0,0 +1,14 @@
+package me.gestionStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockProduitProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockProduitProjApplication.class, args);
+	}
+
+}
+
